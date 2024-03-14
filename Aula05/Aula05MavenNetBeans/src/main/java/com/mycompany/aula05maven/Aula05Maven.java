6e1707249a0ceee16b7a4e568f42e45059e7a903
lucaspvc <lucaspessoalves25@gmail.com>
2024-03-14 10:53:45 -0300

    Aula05Maven

---
+++ Aula05/Aula05MavenNetBeans/src/main/java/com/mycompany/aula05maven/Aula05Maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula05maven;
+
+/**
+ *
+ * @author 2022.1.08.044
+ */
+public class Aula05Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, cruel Apache NetBenas and Maven World!");
+    }
+}
