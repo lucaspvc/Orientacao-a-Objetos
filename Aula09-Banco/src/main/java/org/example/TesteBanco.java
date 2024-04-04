da74b07a7d9aee229fc4705fdf2c3c5c7f24add5
lucaspvc <lucaspessoalves25@gmail.com>
2024-04-04 11:39:14 -0300

    Aula09

--- Aula09-Banco/src/main/java/org/example/TesteBanco.java
+++ Aula09-Banco/src/main/java/org/example/TesteBanco.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class TesteBanco {
     public static void main(String[] args) {
-
-        System.out.println("Criando o cliente " + Cliente.class.getName());
+        Cliente cliente = new Cliente("Bruno","Henrique");
+        System.out.println("Criando o cliente " + cliente.getNome());
         System.out.println("Criando uma conta com R% 50.000,00 para o cliente " + Cliente.class.getName());
 
     }
